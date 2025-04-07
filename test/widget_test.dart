55a9d5115134f53aaa4d4367d734ade4ef03d1d0
城市星表_之音饲料馆 <155783942+1028867764@users.noreply.github.com>
2025-04-08 04:32:19 +0800

    同上

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:Econor/main.dart';
+import 'package:econor/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
